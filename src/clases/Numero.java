5d69f691d2d79b90d179d4efc2af44029b13fdef
jhonbg09 <jhon.betancourthg09@gmail.com>
2024-07-21 18:25:38 -0500

    Agregando commit

---
+++ src/clases/Numero.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author informatica
+ */
+public class Numero {
+    private int codigo;
+    private String numero;
+    private Tipo tipo;
+
+    public Numero() {
+        tipo = new Tipo();
+    }
+
+    public Numero(int codigo, String numero, Tipo tipo) {
+        this.codigo = codigo;
+        this.numero = numero;
+        this.tipo = tipo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+}
